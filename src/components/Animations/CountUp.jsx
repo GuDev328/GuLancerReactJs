aa95279682e94af52e419ab788f28844b4ca590d
GuDev <datminiphi@gmail.com>
2025-01-18 17:57:51 +0700

    fix: fix

---
+++ src/components/Animations/CountUp.jsx
@@ -0,0 +1,112 @@
+import { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+
+export default function CountUp({
+  to,
+  from = 0,
+  direction = "up",
+  delay = 0,
+  duration = 2,
+  className = "",
+  startWhen = true,
+  separator = "",
+  onStart,
+  onEnd,
+}) {
+  const ref = useRef(null);
+  const motionValue = useMotionValue(direction === "down" ? to : from);
+
+  const damping = 20 + 40 * (1 / duration);
+  const stiffness = 100 * (1 / duration);
+
+  const springValue = useSpring(motionValue, {
+    damping,
+    stiffness,
+  });
+
+  const isInView = useInView(ref, { once: true, margin: "0px" });
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.textContent = String(direction === "down" ? to : from);
+    }
+  }, [from, to, direction]);
+
+  useEffect(() => {
+    if (isInView && startWhen) {
+      if (typeof onStart === "function") {
+        onStart();
+      }
+
+      const timeoutId = setTimeout(() => {
+        motionValue.set(direction === "down" ? from : to);
+      }, delay * 1000);
+
+      const durationTimeoutId = setTimeout(() => {
+        if (typeof onEnd === "function") {
+          onEnd();
+        }
+      }, delay * 1000 + duration * 1000);
+
+      return () => {
+        clearTimeout(timeoutId);
+        clearTimeout(durationTimeoutId);
+      };
+    }
+  }, [
+    isInView,
+    startWhen,
+    motionValue,
+    direction,
+    from,
+    to,
+    delay,
+    onStart,
+    onEnd,
+    duration,
+  ]);
+
+  useEffect(() => {
+    const unsubscribe = springValue.on("change", (latest) => {
+      if (ref.current) {
+        const options = {
+          useGrouping: !!separator,
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 0,
+        };
+
+        const formattedNumber = Intl.NumberFormat("en-US", options).format(
+          latest.toFixed(0)
+        );
+
+        ref.current.textContent = separator
+          ? formattedNumber.replace(/,/g, separator)
+          : formattedNumber;
+      }
+    });
+
+    return () => unsubscribe();
+  }, [springValue, separator]);
+
+  return <span className={`${className}`} ref={ref} />;
+}
+CountUp.propTypes = {
+  to: PropTypes.number.isRequired,
+  from: PropTypes.number,
+  direction: PropTypes.oneOf(["up", "down"]),
+  delay: PropTypes.number,
+  duration: PropTypes.number,
+  className: PropTypes.string,
+  startWhen: PropTypes.bool,
+  separator: PropTypes.string,
+  onStart: PropTypes.func,
+  onEnd: PropTypes.func,
+};
+
+export const CountUpFromZero = ({ to }) => (
+  <CountUp from={0} to={to || 0} separator="," direction="up" duration={0.5} />
+);
+CountUpFromZero.propTypes = {
+  to: PropTypes.number,
+};
--- src/pages/FindJob/components/Banner.jsx
+++ src/pages/FindJob/components/Banner.jsx
@@ -1,112 +1,115 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Chip, Input, Option, Select } from "@material-tailwind/react";
 import ChartTopTech from "./ChartTopTech";
 import PropTypes from "prop-types";
 import projectServices from "../../../services/projectServices";
+import CountUp from "@/components/Animations/CountUp";
+import { CountUpFromZero } from "../../../components/Animations/CountUp";
 
 const Banner = ({ setDataSearch }) => {
-    const [keySearch, setKeySearch] = React.useState("");
-    const [market, setMarket] = React.useState([]);
-    const [dataChart, setDataChart] = React.useState([]);
+  const [keySearch, setKeySearch] = React.useState("");
+  const [market, setMarket] = React.useState([]);
+  const [dataChart, setDataChart] = React.useState([]);
 
-    const handleSearch = () => {
-        setDataSearch((prev) => ({ ...prev, key: keySearch }));
-    };
-    const fetchMarket = async () => {
-        const res = await projectServices.getMarket();
-        setMarket(res.result);
-        setDataChart(res.result.techResult);
-    };
-    useEffect(() => {
-        fetchMarket();
-    }, []);
-    return (
-        <div className="flex h-[350px] justify-center  bg-gradient-to-r from-blue-300 to-cyan-300">
-            <div className="w-[100%] lg:w-[50%]">
-                <div className="text-white text-[27px] flex flex-col mt-14 items-center font-medium">
-                    <p className="min-w-[75%] first-letter:text-[60px] leading-none">
-                        Chào ngày mới,
-                    </p>
-                    <p className="min-w-[75%] leading-6 mb-3">
-                        3424 dự án đang đợi bạn ...
-                    </p>
-                </div>
-
-                <div className="flex  justify-center ">
-                    <div className="min-w-[60%] !relative">
-                        <input
-                            value={keySearch}
-                            onChange={(e) => setKeySearch(e.target.value)}
-                            className=" border  min-w-[100%] h-12 pl-14 text-[12px] text-medium border-gray-300 rounded-md bg-white  "
-                            placeholder="Tìm theo dự án, kỹ năng, ..."
-                        />
-                        <i className=" !absolute text-[25px] left-3 top-[11px] fa-regular text-main fa-magnifying-glass mr-2"></i>
-                    </div>
-
-                    <Button
-                        color="blue"
-                        buttonType="filled"
-                        className=" w-[125px] ml-2 h-12 font-bold"
-                        onClick={handleSearch}
-                    >
-                        Tìm kiếm
-                    </Button>
-                </div>
-            </div>
-
-            <div className="w-[50%] content-center hidden lg:block">
-                <div className="w-[90%] text-white flex flex-col h-[300px] rounded-3xl bg-[#193C61]">
-                    <div className="flex pt-3 pl-4">
-                        <i className="text-[23px]  fa-duotone fa-magnifying-glass-waveform"></i>
-                        <p className="font-bold text-[14px]">
-                            Thị trường làm việc :
-                        </p>
-                    </div>
-
-                    <div className="flex mb-3 justify-around text-[13px]">
-                        <p></p>
-                        <p></p>
-                        <p>Dự án đang tuyển: </p>
-                        <p>{market?.projectResult?.total}</p>
-                        <div className="bg-white w-[2px] h-[20px]"></div>
-                        <p>Dự án mới hôm nay:</p>
-                        <p>{market?.projectResult?.today}</p>
-                        <p></p>
-                        <p></p>
-                    </div>
-                    <div className="bg-white m-auto my-0 mb-3 w-[80%] h-[2px]"></div>
-
-                    <div className="flex justify-between items-center mr-20">
-                        <div className="flex text-[14px] pl-4">
-                            <i className="text-[23px] fa-solid fa-chart-waterfall mr-2"></i>
-                            <p>Nhu cầu tuyển dụng theo</p>
-                        </div>
-                        <div className="w-[200px] text-white ">
-                            <Select
-                                className="text-[14px] text-white font-sans"
-                                variant="static"
-                                onChange={(e) => {
-                                    if (e === 0)
-                                        setDataChart(market.techResult);
-                                    else setDataChart(market.fieldResult);
-                                }}
-                                value={0}
-                            >
-                                <Option value={0}>Theo công nghệ</Option>
-                                <Option value={1}>Theo lĩnh vực</Option>
-                            </Select>
-                        </div>
-                    </div>
-
-                    <ChartTopTech data={dataChart} />
-                </div>
-            </div>
+  const handleSearch = () => {
+    setDataSearch((prev) => ({ ...prev, key: keySearch }));
+  };
+  const fetchMarket = async () => {
+    const res = await projectServices.getMarket();
+    setMarket(res.result);
+    setDataChart(res.result.techResult);
+  };
+  useEffect(() => {
+    fetchMarket();
+  }, []);
+  return (
+    <div className="flex h-[350px] justify-center  bg-gradient-to-r from-blue-300 to-cyan-300">
+      <div className="w-[100%] lg:w-[50%]">
+        <div className="text-white text-[27px] flex flex-col mt-14 items-center font-medium">
+          <p className="min-w-[75%] first-letter:text-[60px] leading-none">
+            Chào ngày mới,
+          </p>
+          <p className="min-w-[75%] leading-6 mb-3">
+            <CountUpFromZero to={market?.projectResult?.total || 0} /> dự án
+            đang đợi bạn ...
+          </p>
         </div>
-    );
+
+        <div className="flex  justify-center ">
+          <div className="min-w-[60%] !relative">
+            <input
+              value={keySearch}
+              onChange={(e) => setKeySearch(e.target.value)}
+              className=" border  min-w-[100%] h-12 pl-14 text-[12px] text-medium border-gray-300 rounded-md bg-white  "
+              placeholder="Tìm theo dự án, kỹ năng, ..."
+            />
+            <i className=" !absolute text-[25px] left-3 top-[11px] fa-regular text-main fa-magnifying-glass mr-2"></i>
+          </div>
+
+          <Button
+            color="blue"
+            buttonType="filled"
+            className=" w-[125px] ml-2 h-12 font-bold"
+            onClick={handleSearch}
+          >
+            Tìm kiếm
+          </Button>
+        </div>
+      </div>
+
+      <div className="w-[50%] content-center hidden lg:block">
+        <div className="w-[90%] text-white flex flex-col h-[300px] rounded-3xl bg-[#193C61]">
+          <div className="flex pt-3 pl-4">
+            <i className="text-[23px]  fa-duotone fa-magnifying-glass-waveform"></i>
+            <p className="font-bold text-[14px]">Thị trường làm việc :</p>
+          </div>
+
+          <div className="flex mb-3 justify-around text-[13px]">
+            <p></p>
+            <p></p>
+            <p>Dự án đang tuyển: </p>
+            <p>
+              {" "}
+              <CountUpFromZero to={market?.projectResult?.total} />
+            </p>
+            <div className="bg-white w-[2px] h-[20px]"></div>
+            <p>Dự án mới hôm nay:</p>
+            <CountUpFromZero to={market?.projectResult?.today} />
+            <p></p>
+            <p></p>
+          </div>
+          <div className="bg-white m-auto my-0 mb-3 w-[80%] h-[2px]"></div>
+
+          <div className="flex justify-between items-center mr-20">
+            <div className="flex text-[14px] pl-4">
+              <i className="text-[23px] fa-solid fa-chart-waterfall mr-2"></i>
+              <p>Nhu cầu tuyển dụng theo</p>
+            </div>
+            <div className="w-[200px] text-white ">
+              <Select
+                className="text-[14px] text-white font-sans"
+                variant="static"
+                onChange={(e) => {
+                  if (e === 0) setDataChart(market.techResult);
+                  else setDataChart(market.fieldResult);
+                }}
+                value={0}
+              >
+                <Option value={0}>Theo công nghệ</Option>
+                <Option value={1}>Theo lĩnh vực</Option>
+              </Select>
+            </div>
+          </div>
+
+          <ChartTopTech data={dataChart} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 Banner.propTypes = {
-    setDataSearch: PropTypes.func.isRequired,
+  setDataSearch: PropTypes.func.isRequired,
 };
 
 export default Banner;
